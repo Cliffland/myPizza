a183404255cd331a72c7be503caa02ca5898fb78
sungu <jcliffland@gmail.com>
2022-03-19 18:28:40 +0300

    added fun hide table

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+$document(document).ready(function(){
+    $('table').hide();
+    $('.extra-button').hide();
+    $('extra-info').hide();
+    $('.btn .yes').hide();
+    $('.btn .no').hide();
+    $('.extra-info .po').hide();
+})
