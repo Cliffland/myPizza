c7d9a0e2adbff1bf7ecd097ddf994c33094cc2e4
sungu <jcliffland@gmail.com>
2022-03-19 20:17:52 +0300

    added click fun

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,4 +5,19 @@ $document(document).ready(function(){
     $('.btn .yes').hide();
     $('.btn .no').hide();
     $('.extra-info .po').hide();
-})
+
+    $('.btn .order').click(function(){
+        var sizeOfPizza = $('.size option:selected').val();
+        var toppingsOfPizza = $('.toppings option: selected').val();
+        var crustOfPizza = $('.crust option: selected').val();
+        var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+        var order = 1;
+        var grandtotal = 0;
+
+        $('table').show();
+        $('.extra-button').show();
+        $('.btn .order').hide();
+
+        
+    });
+});
