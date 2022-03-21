8cb4e333cd84d00e94639f3404a3a22bd0f2c33a
sungu <jcliffland@gmail.com>
2022-03-21 17:53:08 +0300

    debugged btn order

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,8 +22,8 @@ $(document).ready(function(){
         $('.btn.order').hide();
 
         $('#inputSize').html($('.size option:selected').text() + " - " + sizeOfPizza);
-        $('#inputToppings').html($('.toppings option:selected').text() + "-" + toppingsOfPizza);
-        $('#inputCrust').html($('.crust option:selected').text() + "-" + crustOfPizza);
+        $('#inputToppings').html($('.toppings option:selected').text() + " - " + toppingsOfPizza);
+        $('#inputCrust').html($('.crust option:selected').text() + " - " + crustOfPizza);
         $('#total').html(total);
         
         function Pizza(size, toppings, crust, total, orderNo){
@@ -33,7 +33,7 @@ $(document).ready(function(){
             this.total = total;
             this.orderNo = orderNo;
         }
-
+        
         $('.btn.more-pizza').click(function(){
             var sizeOfPizza = $('.size option:selected').val();
             var toppingsOfPizza = $('.toppings option:selected').val();
@@ -43,9 +43,12 @@ $(document).ready(function(){
             grandTotal = grandTotal + total;
 
             var newPizza = new Pizza(sizeOfPizza, toppingsOfPizza, crustOfPizza, total,order);
+        
             var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text() + "-" + newPizza.size + '</td><td id="toppings">' + $(".toppings option:selected").text() + "-" + newPizza.toppings + '</td><td id="crust">' + $(".crust option:selected").text() + "-" + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
+
             $("#pizza").append(newRow);
         });
+
         $('.btn.check-out').click(function(){
             $('.btn.more-pizza').hide();
             $('.btn.check-out').hide();
@@ -58,23 +61,23 @@ $(document).ready(function(){
             $('.extra-info h5 span').html(grandTotal);
         });
         $('.btn.yes').click(function(){
-            $('.extra-info h5').hide();
+            $('.extra-info h6').hide();
             $('.btn.yes').hide();
             $('.btn.no').hide();
             $('.extra-info .location').show();
             $('.extra-info h5 span').html(grandTotal + 200);
         });
         $('.btn.no').click(function(){
-            $('.extra-info h5').hide();
+            $('.extra-info h6').hide();
             $('.btn.yes').hide();
             $('.btn.no').hide();
-            $('.extra-info .location').show();
+            $('.extra-info .location').hide();
         });
         $('.btn.complete').click(function(){
             var location = $('.extra-info .location input').val();
-            $('.extra-info.po').show();
+            $('.extra-info .po').show();
             $('.extra-info .location').hide();
-            $('.extra-info.po span').html(location);
+            $('.extra-info .po span').html(location);
         });
     });
 });
